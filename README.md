862e52e1b92157afb3dd15659736451e5a3bb866
illubaby <137404278+illubaby@users.noreply.github.com>
2024-03-23 12:48:34 +0700

    Update description and buid the first base diagram for layout 3

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-Android app "Smart Office" for the Multidisciplinary Project \
-Figma's desgin at https://www.figma.com/file/QpsUPRlKy4oHXpDoGjBVgU/CO3107?type=design&node-id=0%3A1&mode=design&t=xQfuj5w1wrR6NNpd-1 \
-Yolo:Bit program at https://app.ohstem.vn/#!/share/yolobit/2dgPsyiTBp39Yv33C1ZmFCHmoUT
+# Smart Office - Android App
+
+This repository hosts the Android application "Smart Office," developed as part of the Multidisciplinary Project.
+
+## Design
+
+The UI/UX design for Smart Office is available on Figma. You can view the design mockups and flow here:
+[Smart Office Figma Design](https://www.figma.com/file/QpsUPRlKy4oHXpDoGjBVgU/CO3107?type=design&node-id=0%3A1&mode=design&t=xQfuj5w1wrR6NNpd-1)
+
+## Yolo:Bit Program
+
+The Yolo:Bit program used for controlling various IoT devices can be accessed at the following link:
+[Yolo:Bit Program on OhStem](https://app.ohstem.vn/#!/share/yolobit/2dgPsyiTBp39Yv33C1ZmFCHmoUT)
+
+### Demo Yolo:Bit
+
+A video demonstration of the Yolo:Bit kit program can be found below. This video showcases features such as:
+
+- Voice commands for turning on the LED and fan
+- Face recognition for opening the door
+- Door lock with password protection
+
+[Watch the Demo Video](https://drive.google.com/drive/folders/1-E5J9AcZsORWQllVoYNEnBiVWfDGg5-n?usp=sharing)
+
+---
+
+Please feel free to contribute to the project or suggest any improvements.
+
+Thank you for visiting our repository!
--- app/src/main/java/com/example/testsuite/ThirdActivity.java
+++ app/src/main/java/com/example/testsuite/ThirdActivity.java
@@ -1,17 +1,70 @@
 package com.example.testsuite;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.androidplot.xy.CatmullRomInterpolator;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PanZoom;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYGraphWidget;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.util.Arrays;
+
 public class ThirdActivity extends AppCompatActivity {
     Button btn0, btn1, btn2, btn3;
-
+    XYPlot plot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
+        plot = findViewById(R.id.plot);
+
+        //Create a arrays of y-value to plot:
+        final Number[] domainLabels = {1,2,3,6,7,8,9,10,13,14};
+        Number[] series1Numbers = {1,4,2,8,88,16,8,32,16,64};
+        Number[] series2Numbers = {2,6,3,12,8,0,17,64,20,40};
+
+        // Turn the above arrays into XYSeries
+        XYSeries series1 = new SimpleXYSeries(Arrays.asList(series1Numbers),
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"Series 1");
+        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers),
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"Series 2");
+
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(Color.RED,Color.GREEN,null,null);
+        LineAndPointFormatter series2Format = new LineAndPointFormatter(Color.YELLOW,Color.BLUE,null,null);
+
+        series1Format.setInterpolationParams(new CatmullRomInterpolator.Params(10,
+                CatmullRomInterpolator.Type.Centripetal));
+        series2Format.setInterpolationParams(new CatmullRomInterpolator.Params(10,
+                CatmullRomInterpolator.Type.Centripetal));
+
+        plot.addSeries(series1,series1Format);
+        plot.addSeries(series2,series2Format);
+
+        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                int i = Math.round( ((Number)obj).floatValue() );
+                return toAppendTo.append(domainLabels[i]);
+            }
+
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+            }
+        });
+
+        PanZoom.attach(plot);
+
         btn0 = (Button) findViewById(R.id.btn0);
         btn0.setOnClickListener(e -> {
             Intent intent = new Intent(this, FirstActivity.class);
