fcb9a4a5f640e3977232a975d4212b9d5e1fc448
bmV3Qmll <xthanh051423@gmail.com>
2024-03-26 23:55:44 +0700

    Add database feature

--- app/src/main/java/com/example/testsuite/FourthActivity.java
+++ app/src/main/java/com/example/testsuite/FourthActivity.java
@@ -3,10 +3,22 @@ package com.example.testsuite;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.StringReader;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class FourthActivity extends AppCompatActivity {
     Button btn0, btn1, btn2, btn3;
+    TextView txtMysql;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +39,13 @@ public class FourthActivity extends AppCompatActivity {
             Intent intent = new Intent(this, ThirdActivity.class);
             startActivity(intent);
         });
+        txtMysql = (TextView) findViewById(R.id.txt_mysql);
+        String url = "http://10.0.2.2/CO3107/index.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> txtMysql.setText(response),
+                error -> txtMysql.setText(error.toString())
+        );
+        requestQueue = Volley.newRequestQueue(FourthActivity.this);
+        requestQueue.add(stringRequest);
     }
 }
